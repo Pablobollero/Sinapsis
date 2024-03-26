0d696414fb1940aa0829bab6c58ea7bdabcdd4d5
Pablo Bollero <pablo.bolleroh@gmail.com>
2024-03-26 23:24:26 +0000

    Auth done

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,2 +1,2 @@
 export const base_auth_url = "https://identitytoolkit.googleapis.com/v1/"
-export const api_key = "AIzaSyBB5INY0nM4Rt5Y5fbR1b_rswN-hvP7_7k"
+export const api_key = "AIzaSyApca3KZvv-2UWI1f9dVY2rdKxZnyBYN1Y"
